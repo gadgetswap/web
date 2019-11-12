4f490cd8cadd98084c00876a76a77e0281b55b00
Ali Zahid <ali.zahid@live.com>
2019-11-12 16:52:11 +0400

    Styles updated

--- components/nav-link.tsx
+++ components/nav-link.tsx
@@ -3,9 +3,30 @@ import { useRouter } from 'next/router'
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components'
 
+import { colors } from '../assets/styles'
+
 const Main = styled.a`
+  position: relative;
+
+  &:before {
+    content: '';
+    height: 1em;
+    margin: 0.125em 0 0 -0.125em;
+    position: absolute;
+    transition: 0.2s;
+    width: calc(100% - 4em);
+    transform: scale(0.1);
+    z-index: -1;
+  }
+
+  &:hover,
   &.active {
-    font-weight: 700;
+    font-weight: 600;
+
+    &:before {
+      background: ${colors.accent};
+      transform: skew(-5deg, -5deg) scale(1);
+    }
   }
 `
 
