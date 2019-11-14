2af120393a206a3908175aeff3c8c029f7fb38e0
Ali Zahid <ali.zahid@live.com>
2019-11-14 12:23:51 +0400

    Styles updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ module.exports = {
       },
       flex: {
         'gadget-1': '1 0 calc(100% - 2em)',
-        'gadget-2': '1 0 calc(50% - 2em)',
-        'gadget-3': '1 0 calc(100% / 3 - 2em)'
+        'gadget-2': '0 0 calc(50% - 2em)',
+        'gadget-3': '0 0 calc(100% / 3 - 2em)'
       },
       fontFamily: {
         sans: ['Inter var']
