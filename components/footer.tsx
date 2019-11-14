843811db985cf88115c475668d5bc45cecc3d881
Ali Zahid <ali.zahid@live.com>
2019-11-14 13:51:39 +0400

    Footer updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,10 @@ import React, { FunctionComponent } from 'react'
 export const Footer: FunctionComponent = () => (
   <footer className="m-8">
     <p className="text-gray-500 m-0 leading-none">
-      &copy; {new Date().getFullYear()} / GadgetSwap
+      &copy; {new Date().getFullYear()} / GadgetSwap /&nbsp;
+      <a href="http://github.com/gadgetswap" target="_blank">
+        GitHub
+      </a>
     </p>
   </footer>
 )
