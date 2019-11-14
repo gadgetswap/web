d3197baf25baca96a9d100ea84aed103a45082b0
Ali Zahid <ali.zahid@live.com>
2019-11-14 18:59:13 +0400

    Bug fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,10 @@ export const Footer: FunctionComponent = () => (
   <footer className="m-8">
     <p className="text-gray-500 m-0 leading-none">
       &copy; {new Date().getFullYear()} / GadgetSwap /&nbsp;
-      <a href="http://github.com/gadgetswap" target="_blank">
+      <a
+        href="http://github.com/gadgetswap"
+        target="_blank"
+        rel="noopener noreferrer">
         GitHub
       </a>
     </p>
